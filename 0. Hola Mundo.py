18f34d36daa69b5ac86eb1952ad66578b6e5cb0a
joancordero481-tech <joancordero481@gmail.com>
2025-10-17 11:06:25 -0400

    Add files via upload

---
+++ 0. Hola Mundo.py
@@ -0,0 +1,28 @@
+numero1= int(input("cual es el numero 1:  "))
+numero2= int(input("cual es el numero 2:  "))
+
+sum= numero1 + numero2
+resta= numero1 - numero2
+division= numero1 / numero2
+multiplicacion = numero1 * numero2
+
+print (f"los numeros  {numero1} y {numero2} sus resultados son: suma {sum}")
+       
+
+
+grados = int(input("cuantos grados hay?: "))
+f = ( grados * 9/5) + 32
+print(f" el resultado de la coversion es {f}")
+
+
+numero1 = int(input("cual es el primer numero"))
+numero2 = int(input("cual es el segundo numero"))
+
+d = numero1 > numero2
+i = numero1 == numero2
+a = numero2 >= numero1
+
+print(f"los resulatodos son {d} {i} {a}")
+
+
+calificacio =
